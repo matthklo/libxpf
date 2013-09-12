02d0d7c93e17d3b2107fb494ccfc8c9c679dd128
Matt HK Lo <hklo.tw@gmail.com>
2013-09-12 17:14:25 +0800

    use STLport header-only

--- build/prepare_unix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cmake -G "Unix Makefiles" ../
-
--- build/prepare_win32.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cmd /C "cd ..\external\STLPort-5.2.1 && configure msvc9 --clean"
-cmd /C "cd ..\external\STLPort-5.2.1 && configure msvc9 --with-dynamic-rtl"
-cmd /C "cd ..\external\STLPort-5.2.1\build\lib && nmake clean install"
-
